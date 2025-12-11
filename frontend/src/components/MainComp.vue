0c14d73d7c8c8dd74ea74be89470d99792b21055
MIGUEL-TGH <123117682+MIGUEL-TGH@users.noreply.github.com>
2025-12-11 01:08:19 -0600

    Avances 2025-12-11 1 am

---
+++ frontend/src/components/MainComp.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>REDIM Micrositio</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'MainComp',
+  data () {
+    return { }
+  },
+  computed: {
+
+  },
+  methods: {
+  },
+  mounted () {
+  },
+  async created () {
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ frontend/src/views/MainView.vue
@@ -0,0 +1,20 @@
+<template>
+  <main-comp />
+</template>
+
+<script>
+import MainComp from '@/components/MainComp.vue'
+// import { mapState } from 'vuex'
+
+export default {
+  name: 'HomeView',
+  components: {
+    MainComp
+  },
+  computed: {
+    // ...mapState([
+    //   'titulo'
+    // ])
+  }
+}
+</script>
